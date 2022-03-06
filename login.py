eb9fb249bee216a04ffe57091430f19f742d3741
EchoHowardLam <lamyipfu@gmail.com>
2022-03-06 18:04:31 +0800

    Update login macro to automatically accept the terms

--- login.py
+++ login.py
@@ -1,9 +1,8 @@
 from splinter.browser import Browser
 from time import sleep
  
-URL = 'https://securelogin.net.cuhk.edu.hk/upload/custom/CUPortal/login.html'
-SID = '1155000000'
-CUHKHOST = '@link.cuhk.edu.hk'
+URL = 'https://securelogin.wlan.cuhk.edu.hk/'
+ACCTNAME = ''
 PASSWORD = '' #remember to change it to your own Onepass password
  
 def main():
@@ -11,9 +10,13 @@ def main():
     br = Browser('chrome', executable_path='./chromedriver', headless=True)
     print('opened')
     br.visit(URL)
-    print('visted')
-    sleep(1)
-    br.fill('user', SID+CUHKHOST)
+    print('visited')
+    sleep(5)
+    br.find_by_xpath('//input[@type="checkbox"]').first.click()
+    br.find_by_name('accept').first.click()
+    print('accept terms')
+    sleep(5)
+    br.fill('user', ACCTNAME)
     br.fill('password', PASSWORD)
     br.find_by_name('submit').first.click()
     sleep(3)
